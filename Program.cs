5a35c56f8049155d650a80393bcc628978909e8c
Levy Sampaio <levysampaio@live.com>
2021-09-27 13:47:08 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CursoCShrap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo t1 = new Triangulo();
+
+            Console.WriteLine("Digite a largura do triangulo: ");
+            t1.Largura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura do triangulo: ");
+            t1.Altura = double.Parse(Console.ReadLine());
+
+            Console.Write(t1);          
+     
+        }
+
+   
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace CursoCShrap
+{
+    class Triangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public double Area()
+        {
+            return Altura * Largura;
+        }
+
+        public double Perimetro()
+        {
+            return (Largura + Altura) * 2.0;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+
+        public override string ToString()
+        {
+            return "Area: " + Area().ToString("F2", CultureInfo.InvariantCulture) + "| Perimetro: " + Perimetro().ToString("F2", CultureInfo.InvariantCulture) + " | Diagonal: " + Diagonal().ToString("F2", CultureInfo.InvariantCulture); 
+        }
+    }
+}
