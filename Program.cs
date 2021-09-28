456e1a4cc5312c4e0ef88fb403dc9ff4e9d1b73c
Levy Sampaio <levysampaio@live.com>
2021-09-28 18:08:58 -0300

    update

--- Program.cs
+++ Program.cs
@@ -6,21 +6,22 @@ namespace CursoCShrap
     {
         static void Main(string[] args)
         {
-            Conta c1 = new Conta();
+            Conta c1;
 
             Console.WriteLine("Entre com o número da conta: ");
-            c1.NumConta = int.Parse(Console.ReadLine());
+            int numConta = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Entre com o titular da conta: ");
-            c1.Cliente = Console.ReadLine();
+            string cliente = Console.ReadLine();
 
             Console.WriteLine("Deseja entrar com um valor inicial? s/n");
             char opcao = char.Parse(Console.ReadLine());
             if (opcao.Equals('s'))
             {
                 Console.WriteLine("Entre com o valor inicial:");
-                c1.Deposito(double.Parse(Console.ReadLine()));
+                double valorInicial = double.Parse(Console.ReadLine());
 
+                c1 = new Conta(cliente, numConta, valorInicial);
                 Console.WriteLine("----------------");
                 Console.WriteLine("Dados da Conta:");
                 Console.WriteLine(c1);
@@ -28,6 +29,7 @@ namespace CursoCShrap
             }
             else
             {
+                c1 = new Conta(cliente, numConta);
                 Console.WriteLine("----------------");
                 Console.WriteLine("Dados da Conta:");
                 Console.WriteLine(c1);
