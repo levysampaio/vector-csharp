381f7f063d8132b773cd00408ff7a4235e96d4be
Levy Sampaio <levysampaio@live.com>
2021-09-29 17:23:28 -0300

    update

---
+++ Estudante.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCShrap
+{
+    class Estudante
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+
+        public Estudante(string nome, string email)
+        {
+
+            Nome = nome;
+            Email = email;
+
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome + " | Email: " + Email;
+        }
+    }
+}
